9b82e1a6eeec7d9f8e89f80783dc1ce055dc6b11
G Surya Narayanan <64247956+Suryanarayanang98@users.noreply.github.com>
2024-11-20 20:09:15 +0530

    Time: 39 ms (36.72%), Space: 16.6 MB (26.28%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,21 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
         y = 0
-        count = 0
         neg_flag = True if x<0 else False
         x = abs(x)
-        length = len(str(x))
         while x>=1:
-            rem = x%10
-            y += rem*10**(length - 1 - count)
-            count+=1
+            y = y*10 + (x%10)
             x = x//10
-            
-        y = -y if neg_flag else y
-        if y>2**31-1 and not neg_flag:
+        
+        if neg_flag:
+            y *= -1
+        
+        if y>2**31 - 1 or y < -2**31:
             return 0
-        if y<-2**31 and neg_flag:
-            # print("here")
-            return 0 
         
         return y
