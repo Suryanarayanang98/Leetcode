56954b76c9a212f0abe8114b01c6b4f10fdc3688
G Surya Narayanan <64247956+Suryanarayanang98@users.noreply.github.com>
2024-12-10 22:52:37 +0530

    Time: 1014 ms (66.59%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,8 +1,4 @@
-with table_1 as (select count(*) as total_users from users)
-Select contest_id, round(sum1/total_users*100,2) as percentage from (
-select contest_id, count(user_id) as sum1
+select contest_id, round(count(user_id)*100/(select count(*) from users),2) as percentage
 from register
 group by contest_id
-) f
-cross join table_1
-order by percentage desc, contest_id
+order by percentage desc,contest_id
